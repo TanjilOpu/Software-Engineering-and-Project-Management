b73896b4c3b0daa650c6bd0ad0c0caa2ffe4c57e
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2025-11-01 23:26:03 +0600

    Add files via upload

---
+++ oop_revise/class_and_object/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.name = "bmw";
+        car1.color="black";
+        car1.price=1000;
+        System.out.println("Car name = " + car1.name);
+        System.out.println("Car color = " + car1.color);
+        System.out.println("Car price = " + car1.price);
+    }
+}
